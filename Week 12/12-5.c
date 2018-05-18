3839784016649cd7d03fc79665e3275fa05d49a4
XuZhixuan <sxxuzhixuan@hotmail.com>
2018-05-18 12:18:03 +0800

    Add 12-5

---
+++ Week 12/12-5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, b;
+
+	if (a < b)
+	{
+		a ^= b;
+		b ^= a;
+		a ^= b;
+	}
+
+	int k = a - b;
+
+	a = (int)(k*(1 + sqrt(5)) / 2.0);
+
+	if (a == b)
+		printf("0\n");
+	else
+		printf("1\n");	
+
+	return 0;
+}

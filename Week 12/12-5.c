ceaabf792ca749de0cba2466c1ddd7f7939c5ad0
XuZhixuan <sxxuzhixuan@hotmail.com>
2018-05-19 21:32:16 +0800

    Hot fix

--- Week 12/12-5.c
+++ Week 12/12-5.c
@@ -5,6 +5,8 @@ int main()
 {
 	int a, b;
 
+	scanf_s("%d%d", &a, &b);
+
 	if (a < b)
 	{
 		a ^= b;
@@ -13,7 +15,7 @@ int main()
 	}
 
 	int k = a - b;
-
+	
 	a = (int)(k*(1 + sqrt(5)) / 2.0);
 
 	if (a == b)

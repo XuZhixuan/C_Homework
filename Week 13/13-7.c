c56f5c11ff596df46e074b6cb1b67fc694b05aac
XuZhixuan <sxxuzhixuan@hotmail.com>
2018-05-24 18:30:22 +0800

    Fix 13-7

--- Week 13/13-7.c
+++ Week 13/13-7.c
@@ -12,30 +12,63 @@ struct Word
 
 struct Word count[100];
 
-void split();
+int split();
+void rank(int length);
 
 int main()
 {
 	gets_s(input, 1000);
 	
-	split();
+	int length = split();
 
+	rank(length);
+
+	printf("%s", count[0].token);
 	return 0;
 }
 
-void split()
+int split()
 {
-	int i = 0;
+	int i = 1;
 
 	char *token;
 	const char *delim = " ";
 	char *next_token;
 
-	do
+	token = strtok_s(input, delim, &next_token);
+	count[0].token = malloc(strlen(token));
+	count[0].token = token;
+
+	while (token != NULL)
 	{
-		token = strtok_s(input, delim, &next_token);
+		token = strtok_s(NULL, delim, &next_token);
+		if (token == NULL)
+		{
+			break;
+		}
 		count[i].token = malloc(strlen(token));
 		count[i].token = token;
+		count[i].length = strlen(token);
 		i++;
-	} while (token != NULL);
+	}
+
+	return i;
+}
+
+void rank(int length)
+{
+	struct Word temp;
+
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = length - 1; j > i; j--)
+		{
+			if (count[j - 1].length < count[j].length)
+			{
+				temp = count[j - 1];
+				count[j - 1] = count[j];
+				count[j] =temp;
+			}
+		}
+	}
 }
